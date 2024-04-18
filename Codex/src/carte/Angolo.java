c5f0224dd533dda16cbefb2db755dabbc86a728e
liz <rossi.lisa.2004@gmail.com>
2024-04-18 10:51:01 +0200

    costruttore classe angolo

--- Codex/src/carte/Angolo.java
+++ Codex/src/carte/Angolo.java
@@ -5,7 +5,33 @@ public class Angolo {
 	private boolean occupato;
 	private Disegno disegno;
 	
-	public Angolo() {
-		
+	public Angolo(boolean fronte, boolean occupato, Disegno disegno) {
+		this.fronte = fronte;
+		this.occupato = occupato; 
+		this.disegno = disegno;
+	}
+
+	public boolean isFronte() {
+		return fronte;
+	}
+
+	public void setFronte(boolean fronte) {
+		this.fronte = fronte;
+	}
+
+	public boolean isOccupato() {
+		return occupato;
+	}
+
+	public void setOccupato(boolean occupato) {
+		this.occupato = occupato;
+	}
+
+	public Disegno getDisegno() {
+		return disegno;
+	}
+
+	public void setDisegno(Disegno disegno) {
+		this.disegno = disegno;
 	}
 }
