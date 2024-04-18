23a40574250d8022f8a03a3334df395b136a01a1
Raul <raulbeltramelli@gmail.com>
2024-04-18 11:17:21 +0200

    costruttore Carta

--- Codex/src/Carte/Carta.java
+++ Codex/src/Carte/Carta.java
@@ -4,11 +4,23 @@ public abstract class Carta {
 	
 	private Angolo angoli[];
 	protected Colore colore;
-	boolean fronte;
+	boolean fronte; //true=carta giocata con fronte visibile, false=carta giocata con il retro
 	private Disegno disegni[];
 	
 	
-	public Carta() {
+	public Carta(Angolo ang[], Colore col, Disegno dis[]) {
+		this.angoli= new Angolo[8];
+		this.disegni = new Disegno[3];
+		fronte=true;
+		
+		for(int i=0; i<this.angoli.length; i++) {
+			this.angoli[i]=ang[i];
+		}
+		for(int i=0; i<this.disegni.length; i++) {
+			this.disegni[i]=dis[i];
+		}
+		this.colore=col;
+		
 		
 	}
 
