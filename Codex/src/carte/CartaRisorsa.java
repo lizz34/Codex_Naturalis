de348d8426d722991f1512639edd812fb9f6f9a4
edoardopirola <edoardo.pirola04@gmail.com>
2024-04-18 11:47:22 +0200

    costruttore carata risorsa

--- Codex/src/carte/CartaRisorsa.java
+++ Codex/src/carte/CartaRisorsa.java
@@ -3,8 +3,18 @@ package Carte;
 public class CartaRisorsa extends Carta{
 	private int punti;
 	
-	public CartaRisorsa() {
+	public CartaRisorsa(Angolo ang[], Colore col, Disegno dis[], int punti) {
+		super (ang, col, dis);
+		this.punti=punti;
 		
+	}
+
+	public int getPunti() {
+		return punti;
+	}
+
+	public void setPunti(int punti) {
+		this.punti = punti;
 	}
 }
 
